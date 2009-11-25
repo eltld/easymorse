67fcb02dedb6c4f14cf3793b23dff1b94e6ec03c
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2009-11-25 03:59:45 +0000

    通过队列获取转换信息

--- trunk/vp/vp-backend/bin/VideoConvertor.groovy
+++ trunk/vp/vp-backend/bin/VideoConvertor.groovy
@@ -2,14 +2,40 @@
 
 import groovy.lang.Grab;
 import org.apache.log4j.*
+import javax.jms.*
+import org.apache.activemq.ActiveMQConnectionFactory
 
 @Grab(group="log4j",module="log4j",version="1.2.12")
-class Convertor{
+@Grab(group="org.apache.activemq",module="activemq-core",version="5.3.0")
+class Convertor implements MessageListener{
 	
 	private static Logger logger=Logger.getLogger(Convertor.class)
 	
 	static{
 		PropertyConfigurator.configure("log4j.properties");				
+	}
+	
+	def connection
+	def session
+	
+	def start(){
+		logger.info("启动视频转换器...")
+		def connectionFactory=new ActiveMQConnectionFactory("tcp://localhost:61616")
+		connection=connectionFactory.createConnection()
+		connection.start()
+		session=connection.createSession(true,Session.AUTO_ACKNOWLEDGE)
+		def destination=session.createQueue("video_convert")
+		def consumer=session.createConsumer(destination)
+		consumer.setMessageListener(this)
+		logger.info("视频转换器启动完毕。")
+	}
+
+	public void onMessage(Message message){
+		logger.debug("收到消息，正文是：${message.text}")
+		def parameters=message.text.split(";")
+		convert(parameters[0],parameters[1])
+		session.commit()
+		logger.debug("提交会话。")
 	}
 	
 	def convert(input,output){
@@ -18,7 +44,7 @@ class Convertor{
 		
 		if(file.exists()){
 			file.delete()
-			logger.debug("delete file ${output}.")
+			logger.debug("删除已经存在的输出文件 ${output}。")
 		}
 		
 		def process="ffmpeg -i ${input} ${output}".execute()
@@ -28,7 +54,4 @@ class Convertor{
 	}
 }
 
-
-def input='test.mov'
-def output='output.mp4'
-new Convertor().convert(input,output)
+new Convertor().start()
