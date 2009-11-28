bc9222bdd019390f68fd62d8009c5a735ea9e5b6
jiessiedyh@gmail.com <jiessiedyh@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2009-11-28 15:59:17 +0000

    增加了初始内容

--- branches/helloworld1.0/src/main/java/easymorse/App.java
+++ branches/helloworld1.0/src/main/java/easymorse/App.java
@@ -1,13 +1,19 @@
 package easymorse;
 
 /**
- * Hello world!
+ * 
+ * @author 邓彦辉 <a href="mailto:jiessiedyh@gmail.com">jiessiedyh@gmail.com</a> 
  *
  */
 public class App 
 {
+	/**
+	 * 
+	 * 
+	 * @param args
+	 */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "你好，世界！Hello World!" );
     }
 }
