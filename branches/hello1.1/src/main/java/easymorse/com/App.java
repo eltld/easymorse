178b8d839909ee129afe8411838f1d6e9834c67c
jiessiedyh@gmail.com <jiessiedyh@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2009-11-29 03:49:33 +0000

    测试代码

--- branches/hello1.1/src/main/java/easymorse/com/App.java
+++ branches/hello1.1/src/main/java/easymorse/com/App.java
@@ -1,5 +1,7 @@
 package easymorse.com;
 
+import java.util.Date;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(new DealDate().deatTheDate(new Date()));
     }
 }
