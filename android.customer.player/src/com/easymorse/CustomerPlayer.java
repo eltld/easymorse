a65b5a73269880337a3e7127d95ac259c2058e08
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2009-12-22 07:07:05 +0000

    使用TabContentFactory生成Tab内容。

--- android.customer.player/src/com/easymorse/CustomerPlayer.java
+++ android.customer.player/src/com/easymorse/CustomerPlayer.java
@@ -3,8 +3,12 @@ package com.easymorse;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TabHost;
+import android.widget.TabHost.TabContentFactory;
 
 public class CustomerPlayer extends TabActivity {
 	@Override
@@ -12,9 +16,20 @@ public class CustomerPlayer extends TabActivity {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		final TabHost tabHost = getTabHost();
-		
+
 		tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("栏目内容")
-				.setContent(new Intent(this, ContentList.class)));
+				.setContent(new TabContentFactory() {
+
+					@Override
+					public View createTabContent(String tag) {
+						ListView listView = new ListView(CustomerPlayer.this);
+						listView.setAdapter(new ArrayAdapter<String>(
+								CustomerPlayer.this,
+								android.R.layout.simple_list_item_1,
+								new String[] { "c1", "c2", "c3" }));
+						return listView;
+					}
+				}));
 
 		tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("栏目列表")
 				.setContent(new Intent(this, ItemList.class)));
