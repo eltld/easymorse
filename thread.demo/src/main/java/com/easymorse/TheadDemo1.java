999078039d42e52263f43759841f55649736062e
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2009-12-29 07:02:14 +0000

    增加多生产者多消费者的示例。

--- thread.demo/src/main/java/com/easymorse/TheadDemo1.java
+++ thread.demo/src/main/java/com/easymorse/TheadDemo1.java
@@ -3,59 +3,18 @@ package com.easymorse;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Thead demo1
- * 
- */
-public class TheadDemo1 {
-
-	public static void main(String[] args) {
-		List<String> contents = new ArrayList<String>();
-		new Thread(new Consumer(contents)).start();
-		new Thread(new Consumer(contents)).start();
-		new Thread(new Producer(contents)).start();
-	}
-}
-
-class Producer implements Runnable {
-
-	private List<String> contents;
-
-	private int count;
-
-	public Producer(List<String> contents) {
-		this.contents = contents;
-	}
-
-	@Override
-	public void run() {
-		while (true) {
-			synchronized (contents) {
-				this.contents.add("s" + (count++));
-				this.contents.notifyAll();
-			}
-			try {
-				Thread.sleep(1000 * 2);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-}
-
 class Consumer implements Runnable {
 
 	private static int consumerCount;
 
 	private List<String> contents;
-	
+
 	private int number;
 
 	public Consumer(List<String> contents) {
 		this.contents = contents;
 		++consumerCount;
-		number=consumerCount;
+		number = consumerCount;
 	}
 
 	@Override
@@ -74,5 +33,55 @@ class Consumer implements Runnable {
 				}
 			}
 		}
+	}
+}
+
+class Producer implements Runnable {
+
+	private static int count;
+
+	private static int producerCount;
+
+	private List<String> contents;
+
+	private int number;
+
+	public Producer(List<String> contents) {
+		this.contents = contents;
+		++producerCount;
+		this.number=producerCount;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			synchronized (contents) {
+				String s="s" + (count++);
+				this.contents.add(s);
+				System.out.println("p" + number + " -> " + s);
+				this.contents.notifyAll();
+			}
+			try {
+				Thread.sleep(1000 * 2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
+
+/**
+ * Thead demo1
+ * 
+ */
+public class TheadDemo1 {
+
+	public static void main(String[] args) {
+		List<String> contents = new ArrayList<String>();
+		new Thread(new Consumer(contents)).start();
+		new Thread(new Consumer(contents)).start();
+		new Thread(new Producer(contents)).start();
+		new Thread(new Producer(contents)).start();
 	}
 }
