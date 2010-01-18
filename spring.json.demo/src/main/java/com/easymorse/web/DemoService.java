dd4a4e738e970975fb66142f819a3773acee1b5d
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2010-01-18 08:56:45 +0000

    json示例，复杂的返回值示例。

--- spring.json.demo/src/main/java/com/easymorse/web/DemoService.java
+++ spring.json.demo/src/main/java/com/easymorse/web/DemoService.java
@@ -1,5 +1,7 @@
 package com.easymorse.web;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DemoService {
 	@RequestMapping("/a.json")
 	public String doService(ModelMap modelMap) {
-		modelMap.put("name", "张三");
+		Pagination pagination = new Pagination();
+		pagination.setPageNo(2);
+		pagination.setResults(new ArrayList<Object>());
+		Book book = new Book();
+		book.setId("1");
+		book.setName("五百年来谁著史");
+		book.setAuthors(new ArrayList<Author>());
+		pagination.getResults().add(book);
+		Author author = new Author();
+		author.setId("1");
+		author.setName("张三");
+		book.getAuthors().add(author);
+		author = new Author();
+		author.setId("2");
+		author.setName("李四");
+		book.getAuthors().add(author);
+
+		modelMap.put("page", pagination);
 		return "list";
 	}
 }
