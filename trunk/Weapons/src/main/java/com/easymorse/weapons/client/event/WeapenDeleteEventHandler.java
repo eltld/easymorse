e96bfe3ccad4d53b1164b8f7ea5d1d8b0bbc73d8
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2010-01-19 11:02:02 +0000

    增加简单的删除功能。

---
+++ trunk/Weapons/src/main/java/com/easymorse/weapons/client/event/WeapenDeleteEventHandler.java
@@ -0,0 +1,7 @@
+package com.easymorse.weapons.client.event;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface WeapenDeleteEventHandler extends EventHandler {
+	public void onDeleteWeapen(WeapenDeleteEvent event);
+}
