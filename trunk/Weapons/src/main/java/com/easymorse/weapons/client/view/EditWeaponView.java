5895ba3251cf80ae1cfe8eb0128aa2777ca39644
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2010-01-21 02:59:47 +0000

    使用spring mvc动态加载图片。

--- trunk/Weapons/src/main/java/com/easymorse/weapons/client/view/EditWeaponView.java
+++ trunk/Weapons/src/main/java/com/easymorse/weapons/client/view/EditWeaponView.java
@@ -3,6 +3,7 @@ package com.easymorse.weapons.client.view;
 import com.easymorse.weapons.client.model.Weapon;
 import com.easymorse.weapons.client.presenter.EditWeaponPresenter;
 import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FileUpload;
@@ -39,17 +40,17 @@ public class EditWeaponView extends Composite implements
 		detailsTable.getColumnFormatter().addStyleName(1, "add-contact-input");
 		name = new TextBox();
 		description = new TextArea();
-		description.setWidth("12em");//TODO 权宜之计，要用formatter做细节布局处理。
+		description.setWidth("12em");// TODO 权宜之计，要用formatter做细节布局处理。
 		description.setVisibleLines(5);
 		initDetailsTable();
 		contentDetailsPanel.add(detailsTable);
-		
+
 		HorizontalPanel menuPanel = new HorizontalPanel();
-	    saveButton = new Button("保存");
-	    cancelButton = new Button("取消");
-	    menuPanel.add(saveButton);
-	    menuPanel.add(cancelButton);
-	    contentDetailsPanel.add(menuPanel);
+		saveButton = new Button("保存");
+		cancelButton = new Button("取消");
+		menuPanel.add(saveButton);
+		menuPanel.add(cancelButton);
+		contentDetailsPanel.add(menuPanel);
 	}
 
 	private void initDetailsTable() {
@@ -58,8 +59,8 @@ public class EditWeaponView extends Composite implements
 		detailsTable.setWidget(1, 0, new Label("介绍"));
 		detailsTable.setWidget(1, 1, description);
 		detailsTable.setWidget(2, 0, new Label("图片"));
-		image=new Image();
-		detailsTable.setWidget(2, 1, image);
+		// image=new Image();
+		// detailsTable.setWidget(2, 1, image);
 		// detailsTable.setWidget(3, 0, new Label("上传图片"));
 		// detailsTable.setWidget(3, 1, new FileUpload());
 		name.setFocus(true);
@@ -94,6 +95,7 @@ public class EditWeaponView extends Composite implements
 	public void setData(Weapon weapon) {
 		this.name.setValue(weapon.getName());
 		this.description.setValue(weapon.getDescription());
-		this.image.setUrl("/images/"+weapon.getId());
+		this.image = new Image("/getImage.do?id=" + weapon.getId());
+		this.detailsTable.setWidget(2, 1, this.image);
 	}
 }
--- trunk/Weapons/src/main/java/com/easymorse/weapons/server/WeaponService.java
+++ trunk/Weapons/src/main/java/com/easymorse/weapons/server/WeaponService.java
@@ -1,7 +1,16 @@
 package com.easymorse.weapons.server;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -80,6 +89,34 @@ public class WeaponService {
 	@ModelAttribute("weapon")
 	public Weapon get(@RequestParam("id") String id) {
 		return find(id);
+	}
+
+	@RequestMapping("/getImage.do")
+	public void getImage(@RequestParam("id") String id,
+			HttpServletRequest request, HttpServletResponse response) {
+		response.setHeader("Pragma", "No-cache");
+		response.setHeader("Cache-Control", "no-cache");
+		response.setDateHeader("Expires", 0);
+
+		if (id == null || id.isEmpty()) {
+			id = "1";
+		}
+
+		try {
+			OutputStream outputStream = response.getOutputStream();
+			BufferedInputStream inputStream = new BufferedInputStream(
+					new FileInputStream(request.getSession()
+							.getServletContext().getRealPath("/images/")
+							+ id));
+			byte[] data = new byte[1024];
+			for (int i = inputStream.read(data); i > 0; i = inputStream
+					.read(data)) {
+				outputStream.write(data, 0, i);
+			}
+			inputStream.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	private void create(Weapon weapon) {
