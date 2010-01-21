0d99133607ff13d852855eb6e2a71e407cc80b6c
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2010-01-21 16:30:11 +0000

    界面实现简单的美化

---
+++ tags/android.customer.player-0.6/src/com/easymorse/PlayMp4.java
@@ -0,0 +1,96 @@
+package com.easymorse;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+public class PlayMp4 extends Activity {
+	private String path = "";
+	private VideoView mVideoView;
+	private static int i = 0;
+	private int width;
+	private int heigh;	
+	private Dialog dialog;
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		//去掉头信息
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		 getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,  
+	                WindowManager.LayoutParams.FLAG_FULLSCREEN);  
+		
+		setContentView(R.layout.videoview);
+		
+        //创建进度条
+		 dialog=ProgressDialog.show(this, "正在加载...", "三枪马上开始");
+		 
+		
+		mVideoView = (VideoView) findViewById(R.id.surface_view);
+		
+		mVideoView.setBackgroundDrawable(getResources().getDrawable(R.drawable.gallery_photo_8));
+		
+		Bundle bundle = this.getIntent().getExtras();
+		
+		//判断手机屏幕的方向
+		DisplayMetrics dm = new DisplayMetrics(); 
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+		width=dm.widthPixels;
+		heigh=dm.heightPixels;
+		if(width/heigh>0)
+		{
+			//横屏
+			path = bundle.getString("widthurl");
+			Log.i("mp4", "heng");
+		}
+		if(width/heigh==0)
+		{
+			//竖屏
+			path = bundle.getString("heighturl");
+			Log.i("mp4", "shu");
+		}
+	
+		mVideoView.setVideoPath(path);
+		MediaController controller = new MediaController(this);
+		mVideoView.setMediaController(controller);
+		mVideoView.requestFocus();
+
+		mVideoView.setOnPreparedListener(new OnPreparedListener() {
+			// 开始播放
+			@Override
+			public void onPrepared(MediaPlayer mp) {
+				mVideoView.setBackgroundColor(Color.argb(0, 0, 255, 0));
+				dialog.dismiss();
+			}
+		});
+		//播放完毕
+		mVideoView.setOnCompletionListener(new OnCompletionListener() {
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				Toast.makeText(PlayMp4.this, "播放完毕，谢谢观看。。。。。", Toast.LENGTH_LONG)
+						.show();
+			}
+		});
+		
+	}
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mVideoView.seekTo(i);
+		mVideoView.start();
+	}
+	@Override
+	protected void onStop() {
+		super.onStop();
+		mVideoView.pause();
+		i = mVideoView.getCurrentPosition();
+	}
+}
