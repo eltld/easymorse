b108872bf8263fc60033e7909244b9e7232fe997
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2010-02-01 06:11:53 +0000

    在gwt增加cookie保存用户名和密码到客户端的功能。目前服务器端有问题，还不能支持。

---
+++ tags/Videos-0.3.2/src/main/java/com/easymorse/videos/client/view/VideoItemView.java
@@ -0,0 +1,85 @@
+package com.easymorse.videos.client.view;
+
+import com.bramosystems.oss.player.core.client.AbstractMediaPlayer;
+import com.bramosystems.oss.player.core.client.LoadException;
+import com.bramosystems.oss.player.core.client.PluginNotFoundException;
+import com.bramosystems.oss.player.core.client.PluginVersionException;
+import com.bramosystems.oss.player.core.client.ui.FlashMediaPlayer;
+import com.easymorse.videos.client.model.VideoItem;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class VideoItemView extends Composite {
+
+	private VideoItem videoItem;
+
+	public VideoItemView(final VideoItem videoItem) {
+		this.videoItem = videoItem;
+
+		HorizontalPanel panel = new HorizontalPanel();
+		initWidget(panel);
+		panel.setSpacing(5);
+
+		Image image = new Image("1.jpg");
+		panel.add(image);
+		image.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				final DialogBox box = new DialogBox();
+				box.setWidth("400");
+
+				VerticalPanel panel = new VerticalPanel();
+//				ArrayList<String> items = new ArrayList<String>();
+//				items.add(new String(GWT.getHostPageBaseURL()
+//						+ "media/big-buck-bunny.mp4"));
+//				items.add(new String(GWT.getHostPageBaseURL()
+//						+ "media/big-buck-bunny.ogv"));
+				AbstractMediaPlayer player = null;
+				try {
+					player = new FlashMediaPlayer(
+							"http://marshal.easymorse.com/videos/test.mp4");
+					panel.add(player);
+				} catch (PluginNotFoundException e) {
+					e.printStackTrace();
+				} catch (LoadException e) {
+					e.printStackTrace();
+				} catch (PluginVersionException e) {
+					e.printStackTrace();
+				}
+
+				box.setText("播放:" + videoItem.getTitle());
+				box.add(panel);
+
+				Button button = new Button("关闭");
+				panel.add(button);
+				button.addClickHandler(new ClickHandler() {
+
+					@Override
+					public void onClick(ClickEvent event) {
+						box.clear();
+						box.hide();
+					}
+				});
+
+				box.setAnimationEnabled(true);
+				box.show();
+			}
+		});
+
+		VerticalPanel verticalPanel = new VerticalPanel();
+		verticalPanel.add(new HTML("<h3>" + videoItem.getTitle() + "</h3>"));
+		verticalPanel.add(new HTML(videoItem.getContent()));
+		panel.add(verticalPanel);
+	}
+
+	public VideoItem getVideoItem() {
+		return videoItem;
+	}
+}
---
+++ tags/Videos-0.3.2/src/main/java/com/easymorse/videos/client/view/VideosView.java
@@ -0,0 +1,130 @@
+package com.easymorse.videos.client.view;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DecoratedTabPanel;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.LazyPanel;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class VideosView extends Composite {
+
+	private Button logoffButton;
+
+	private DialogBox logoffDialogBox;
+
+	private DecoratedTabPanel tabPanel;
+
+	private VerticalPanel browsePanel;
+	
+	private UploadView uploadView;
+
+	public DialogBox getLogoffDialogBox() {
+		return logoffDialogBox;
+	}
+
+	public Button getLogoffButton() {
+		return logoffButton;
+	}
+
+	public VideosView() {
+		VerticalPanel panel = new VerticalPanel();
+		panel.setWidth("500px");
+		panel.setSpacing(5);
+		initWidget(panel);
+
+		HorizontalPanel titlePanel = new HorizontalPanel();
+		titlePanel.setWidth("100%");
+		titlePanel.setSpacing(5);
+		titlePanel.add(new HTML("<h2>Marshal的视频列表</h2>"));
+		titlePanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+
+		Anchor anchor = new Anchor("退出");
+		logoffButton = new Button("退出");
+		anchor.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				logoffDialogBox = new DialogBox();
+				logoffDialogBox.setText("操作提示");
+				logoffDialogBox.setAnimationEnabled(true);
+				logoffDialogBox.setGlassEnabled(true);
+
+				VerticalPanel panel = new VerticalPanel();
+				panel.setWidth("12em");
+				panel.setHeight("3em");
+				panel.add(new HTML("是否真的退出？"));
+				panel.add(new HTML());
+
+				Button cancelButton = new Button("取消");
+				cancelButton.addClickHandler(new ClickHandler() {
+					@Override
+					public void onClick(ClickEvent event) {
+						logoffDialogBox.hide();
+					}
+				});
+
+				HorizontalPanel buttonPanel = new HorizontalPanel();
+				buttonPanel
+						.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+				buttonPanel.add(logoffButton);
+				buttonPanel.add(cancelButton);
+
+				panel.add(buttonPanel);
+				logoffDialogBox.add(panel);
+
+				logoffDialogBox.center();
+				logoffDialogBox.show();
+			}
+		});
+		titlePanel.add(anchor);
+		panel.add(titlePanel);
+
+		tabPanel = new DecoratedTabPanel();
+
+		tabPanel.setWidth("100%");
+		tabPanel.setAnimationEnabled(true);
+
+		tabPanel.add(getBrowseWidget(), "浏览视频");
+		
+		uploadView=new UploadView();
+		
+		tabPanel.add(uploadView, "上传视频");
+		tabPanel.add(new LazyPanel() {
+			@Override
+			protected Widget createWidget() {
+				return new UserView();
+			}
+		}, "用户信息");
+
+		tabPanel.selectTab(0);
+
+		panel.add(tabPanel);
+	}
+
+	public UploadView getUploadView() {
+		return uploadView;
+	}
+
+	public DecoratedTabPanel getTabPanel() {
+		return tabPanel;
+	}
+
+	public  Panel getBrowseWidget() {
+		if (this.browsePanel == null) {
+			VerticalPanel panel = new VerticalPanel();
+			panel.setWidth("100%");
+			this.browsePanel=panel;
+		}
+		return browsePanel;
+	}
+
+}
