033a6da76ed24a4aca701112e3bfd133e58f8c2f
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2010-02-03 03:02:17 +0000

    实现了简单的分页功能。

--- trunk/Videos/src/main/java/com/easymorse/videos/client/presenter/VideosPresenter.java
+++ trunk/Videos/src/main/java/com/easymorse/videos/client/presenter/VideosPresenter.java
@@ -1,9 +1,14 @@
 package com.easymorse.videos.client.presenter;
 
 import com.easymorse.videos.client.event.AccessDeniedEvent;
+import com.easymorse.videos.client.event.BrowseVideoItemsEvent;
+import com.easymorse.videos.client.event.BrowseVideoItemsEventHandler;
 import com.easymorse.videos.client.event.LogOffEvent;
 import com.easymorse.videos.client.event.NeedLoginEvent;
+import com.easymorse.videos.client.event.PlayMideaEvent;
+import com.easymorse.videos.client.event.PlayMideaEventHandler;
 import com.easymorse.videos.client.model.VideoItem;
+import com.easymorse.videos.client.model.VideoItemPagination;
 import com.easymorse.videos.client.view.VideoItemView;
 import com.easymorse.videos.client.view.VideoPlayerView;
 import com.easymorse.videos.client.view.VideosView;
@@ -21,13 +26,10 @@ import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.PushButton;
 import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
 import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteHandler;
 
@@ -51,6 +53,137 @@ public class VideosPresenter implements Presenter, ValueChangeHandler<String> {
 		Window.addResizeHandler(this.videosView);
 
 		History.addValueChangeHandler(this);
+
+		this.handlerManager.addHandler(BrowseVideoItemsEvent.TYPE,
+				new BrowseVideoItemsEventHandler() {
+					@Override
+					public void onBrowseVideoItem(BrowseVideoItemsEvent event) {
+						RequestBuilder builder = new RequestBuilder(
+								RequestBuilder.GET, new StringBuilder().append(
+										"../browse.json?no=").append(
+										event.getPageNo()).toString());
+
+						builder.setCallback(new RequestCallback() {
+
+							@Override
+							public void onResponseReceived(Request request,
+									Response response) {
+								if (response.getStatusCode() == 401) {
+									handlerManager
+											.fireEvent(new NeedLoginEvent());
+								} else {
+									videosView.getBrowseWidget().clear();
+
+									VideoItemPagination pagination = VideoItemPagination
+											.fromJson(response.getText());
+									pageNo = pagination.getNo();
+
+									// VideoItem videoItem = VideoItem
+									// .fromJson("{'id':'1','title':'喜羊羊与灰太狼','content':'国产原创系列电视动画片《喜羊羊与灰太狼》，由广东原创动力文化传播有限公司出品。自2005年6月推出后，陆续在全国近50家电视台热播，在北京、上海、杭州、南京、广州、福州等城市，《喜羊羊与灰太狼》最高收视率达17.3%，大大超过了同时段播出的境外动画片。'}");
+									final VideoItem videoItem = pagination
+											.getResults().get(0);
+									VideoItemView itemView = new VideoItemView(
+											videoItem);
+									itemView.getPlayButton().addClickHandler(
+											new ClickHandler() {
+												@Override
+												public void onClick(
+														ClickEvent event) {
+													handlerManager
+															.fireEvent(new PlayMideaEvent(
+																	videoItem
+																			.getId()));
+												}
+											});
+									videosView.getBrowseWidget().add(itemView);
+									HorizontalPanel pagePanel = new HorizontalPanel();
+
+									pagePanel.setSpacing(5);
+									pagePanel
+											.setHorizontalAlignment(HorizontalPanel.ALIGN_RIGHT);
+									pagePanel.setWidth("100%");
+
+									HorizontalPanel innerPagePanel = new HorizontalPanel();
+									innerPagePanel.setSpacing(5);
+									pagePanel.add(innerPagePanel);
+
+									if (pagination.isPrevious()) {
+										Image image = new Image("/before.gif");
+										image
+												.addClickHandler(new ClickHandler() {
+
+													@Override
+													public void onClick(
+															ClickEvent event) {
+														pageNo--;
+														handlerManager
+																.fireEvent(new BrowseVideoItemsEvent(
+																		pageNo));
+													}
+												});
+										innerPagePanel.add(image);
+									}
+
+									if (pagination.isNext()) {
+										Image image = new Image("/next.gif");
+										image
+												.addClickHandler(new ClickHandler() {
+
+													@Override
+													public void onClick(
+															ClickEvent event) {
+														pageNo++;
+														handlerManager
+																.fireEvent(new BrowseVideoItemsEvent(
+																		pageNo));
+													}
+												});
+										innerPagePanel.add(image);
+									}
+
+									videosView.getBrowseWidget().add(pagePanel);
+
+								}
+							}
+
+							@Override
+							public void onError(Request request, Throwable e) {
+								Window.alert("error");
+							}
+						});
+
+						try {
+							builder.send();
+						} catch (RequestException e1) {
+							e1.printStackTrace();
+						}
+					}
+				});
+
+		this.handlerManager.addHandler(PlayMideaEvent.TYPE,
+				new PlayMideaEventHandler() {
+					@Override
+					public void onPlayMidea(PlayMideaEvent event) {
+						if (videosView.getTabPanel().getTabBar().getTabCount() < 4) {
+							VideoPlayerView playerView = new VideoPlayerView();
+							videosView.getTabPanel()
+									.insert(playerView, "播放", 3);
+							playerView.getCloseButton().addClickHandler(
+									new ClickHandler() {
+										@Override
+										public void onClick(ClickEvent event) {
+											videosView.getTabPanel().remove(3);
+											videosView.getTabPanel()
+													.getTabBar().selectTab(0);
+										}
+									});
+
+							videosView.getTabPanel().getTabBar().selectTab(3);
+						} else {
+							videosView.getTabPanel().getTabBar().selectTab(3);
+						}
+					}
+				});
 
 		this.videosView.getLogoffButton().addClickHandler(new ClickHandler() {
 			@Override
@@ -143,88 +276,10 @@ public class VideosPresenter implements Presenter, ValueChangeHandler<String> {
 		container.add(videosView);
 	}
 
+	private int pageNo = 1;
+
 	private void getBrowseData() {
-		RequestBuilder builder = new RequestBuilder(RequestBuilder.GET,
-				"../browse.json");
-
-		builder.setCallback(new RequestCallback() {
-
-			@Override
-			public void onResponseReceived(Request request, Response response) {
-				if (response.getStatusCode() == 401) {
-					handlerManager.fireEvent(new NeedLoginEvent());
-				} else {
-					videosView.getBrowseWidget().clear();
-					VideoItem videoItem = VideoItem
-							.fromJson("{'id':'1','title':'喜羊羊与灰太狼','content':'国产原创系列电视动画片《喜羊羊与灰太狼》，由广东原创动力文化传播有限公司出品。自2005年6月推出后，陆续在全国近50家电视台热播，在北京、上海、杭州、南京、广州、福州等城市，《喜羊羊与灰太狼》最高收视率达17.3%，大大超过了同时段播出的境外动画片。'}");
-					VideoItemView itemView = new VideoItemView(videoItem);
-					itemView.getPlayButton().addClickHandler(
-							new ClickHandler() {
-								@Override
-								public void onClick(ClickEvent event) {
-									if (videosView.getTabPanel().getTabBar()
-											.getTabCount() < 4) {
-										VideoPlayerView playerView = new VideoPlayerView();
-										videosView.getTabPanel().insert(
-												playerView, "播放", 3);
-										playerView.getCloseButton()
-												.addClickHandler(
-														new ClickHandler() {
-															@Override
-															public void onClick(
-																	ClickEvent event) {
-																videosView
-																		.getTabPanel()
-																		.remove(
-																				3);
-																videosView
-																		.getTabPanel()
-																		.getTabBar()
-																		.selectTab(
-																				0);
-															}
-														});
-
-										videosView.getTabPanel().getTabBar()
-												.selectTab(3);
-									} else {
-										videosView.getTabPanel().getTabBar()
-												.selectTab(3);
-									}
-								}
-							});
-					videosView.getBrowseWidget().add(itemView);
-					HorizontalPanel pagePanel = new HorizontalPanel();
-
-					pagePanel.setSpacing(5);
-					pagePanel
-							.setHorizontalAlignment(HorizontalPanel.ALIGN_RIGHT);
-					pagePanel.setWidth("100%");
-
-					HorizontalPanel innerPagePanel = new HorizontalPanel();
-					innerPagePanel.setSpacing(5);
-					pagePanel.add(innerPagePanel);
-
-					innerPagePanel.add(new Image("/before.gif"));
-					innerPagePanel.add(new Image("/next.gif"));
-
-					videosView.getBrowseWidget().add(pagePanel);
-
-				}
-			}
-
-			@Override
-			public void onError(Request request, Throwable e) {
-				Window.alert("error");
-			}
-		});
-
-		try {
-			builder.send();
-		} catch (RequestException e1) {
-			e1.printStackTrace();
-		}
-
+		handlerManager.fireEvent(new BrowseVideoItemsEvent(pageNo));
 	}
 
 }
