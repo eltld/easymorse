e96f589fbdcf94c94de37c8a38f4643824b15488
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2010-02-04 03:35:51 +0000

    实现保存和选择图片的界面交互。上一个版本有bug，对话框只能出现一次。

---
+++ tags/Videos-0.4.3/src/main/java/com/easymorse/videos/client/presenter/UploadDialogPresenter.java
@@ -0,0 +1,143 @@
+package com.easymorse.videos.client.presenter;
+
+import com.easymorse.videos.client.event.BrowseVideoItemsEvent;
+import com.easymorse.videos.client.event.UploadCompleteEvent;
+import com.easymorse.videos.client.event.UploadCompleteEventHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
+
+public class UploadDialogPresenter implements Presenter {
+
+	private HandlerManager handlerManager;
+
+	private FormPanel formPanel;
+
+	private SubmitEvent event;
+
+	private DialogBox box;
+
+	public UploadDialogPresenter(HandlerManager handlerManager,
+			FormPanel formPanel, SubmitEvent event) {
+		this.handlerManager = handlerManager;
+		this.formPanel = formPanel;
+		this.event = event;
+
+		this.bind();
+	}
+
+	private void bind() {
+		this.handlerManager.addHandler(UploadCompleteEvent.TYPE,
+				new UploadCompleteEventHandler() {
+					@Override
+					public void onUploadComplete(UploadCompleteEvent event) {
+						box.clear();
+						VerticalPanel panel = new VerticalPanel();
+						box.add(panel);
+						int width = 200;
+						panel.setWidth(width + "px");
+						panel.setSpacing(5);
+						panel.add(new HTML("上传成功。"));
+						Button button = new Button("下一步");
+						button.addClickHandler(new ClickHandler() {
+							@Override
+							public void onClick(ClickEvent event) {
+								chooseImage();
+							}
+
+							
+						});
+						panel.add(button);
+					}
+				});
+	}
+	
+	protected void chooseImage() {
+		box.clear();
+		HorizontalPanel panel = new HorizontalPanel();
+		panel.setSpacing(5);
+		box.add(panel);
+		int width=300;
+		panel.setWidth(width+"px");
+		setPosition(width);
+		
+		panel.add(new Image("1.jpg"));
+		
+		VerticalPanel buttonPanel=new VerticalPanel();
+		panel.add(buttonPanel);
+		buttonPanel.setSpacing(5);
+		buttonPanel.add(new Button("上一帧"));
+		buttonPanel.add(new Button("下一帧"));
+		
+		Button confirmButton=new Button("确　定");
+		confirmButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				box.hide();
+				formPanel.reset();
+				handlerManager.fireEvent(new BrowseVideoItemsEvent(1));
+			}
+		});
+		buttonPanel.add(confirmButton);
+	}
+
+	@Override
+	public void go(HasWidgets container) {
+		this.createDialogBox().show();
+	}
+
+	private DialogBox createDialogBox() {
+		box = new DialogBox();
+		box.setText("操作提示");
+		box.setAnimationEnabled(true);
+		box.setGlassEnabled(true);
+		setPosition(200);
+		setOnSubmitView(box);
+		return box;
+	}
+
+	private void setOnSubmitView(final DialogBox box) {
+		VerticalPanel panel = new VerticalPanel();
+		box.add(panel);
+		int width = 200;
+		panel.setWidth(width + "px");
+		panel.setSpacing(5);
+		HorizontalPanel innerPanel = new HorizontalPanel();
+		innerPanel.setSpacing(5);
+		innerPanel.add(new Image("loading.gif"));
+		innerPanel.add(new HTML("正在上传...."));
+		panel.add(innerPanel);
+
+		Button button = new Button("取消");
+		button.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent clickEvent) {
+				event.cancel();
+				box.hide();
+			}
+		});
+		panel.add(button);
+
+		this.setPosition(width);
+	}
+
+	protected void setPosition(int width) {
+		int left = (formPanel.getOffsetWidth() - width) / 2
+				+ formPanel.getAbsoluteLeft();
+		int top = (formPanel.getOffsetHeight() - width / 2) / 2
+				+ formPanel.getAbsoluteTop();
+
+		box.setPopupPosition(left, top);
+
+	}
+
+}
---
+++ tags/Videos-0.4.3/src/main/java/util/dao/HibernateHandler.java
@@ -0,0 +1,7 @@
+package util.dao;
+
+import org.hibernate.Criteria;
+
+public interface HibernateHandler {
+	public void setCriteria(Criteria criteria);
+}
