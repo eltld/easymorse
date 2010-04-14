8bfc46bae9b31325d26eb314a1f06425bcc36301
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2010-04-14 08:31:46 +0000

    第一个简版

--- Weapons/src/main/java/com/easymorse/weapons/client/event/WeaponUpdatedEventHandler.java
+++
@@ -1,7 +0,0 @@
-package com.easymorse.weapons.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface WeaponUpdatedEventHandler extends EventHandler {
-	void onContactUpdated(WeaponUpdatedEvent event);
-}
