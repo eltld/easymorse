062a5cd664f748a58c71f2294883e73236447fcd
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2010-04-21 03:43:45 +0000

    增加了简单的hiberntate实现，使用当前的hibernate 3.5.1-final。

---
+++ tags/gwt.rpc.demo-0.2/src/main/java/com/easymorse/rpc/server/GreetingServiceImpl.java
@@ -0,0 +1,33 @@
+package com.easymorse.rpc.server;
+
+import org.hibernate.Session;
+
+import com.easymorse.rpc.beans.User;
+import com.easymorse.rpc.client.GreetingService;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class GreetingServiceImpl extends RemoteServiceServlet implements
+		GreetingService {
+
+	public String greetServer(String input) {
+		String serverInfo = getServletContext().getServerInfo();
+		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
+		return "Hello, " + input + "!<br><br>I am running " + serverInfo
+				+ ".<br><br>It looks like you are using:<br>" + userAgent;
+	}
+
+	@Override
+	public User helloUser(String userName) {
+		User user = new User();
+		user.setName(userName);
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+		return user;
+	}
+}
