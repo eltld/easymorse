f7cbb53825bb8b9772244ab944671b6bf45a9933
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2010-04-28 04:27:36 +0000

    修改了部分bug，功能未做改变。

--- trunk/chrome.extension.mldonkey/main.js
+++ trunk/chrome.extension.mldonkey/main.js
@@ -1,11 +1,15 @@
 var url=null;
 
-chrome.extension.sendRequest({getParam:"url"}, function(response){
-	url=response.url;
-});
-
 $('a[href^=ed2k]').click(function(){
-	window.alert("使用mldonkey下载，url: "+this.href);
-	window.open(url+"/submit?q="+escape(this.href),"mldonkey","width=300,height=200");
+	
+	chrome.extension.sendRequest({getParam:"url"}, function(response){
+		url=response.url;
+	});
+	
+	if(url==null || url==""){
+		window.alert("mldonkey服务器地址为空。请设置选项中的url为mldonkey服务器地址。");
+	}else{
+		window.open(url+"/submit?q="+escape(this.href),"mldonkey","width=300,height=200");
+	}
 });
 
