c563949522f5317b517bb4f991b88ac9f894567e
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2010-05-10 12:55:07 +0000

    增加对torrent后缀链接的支持。解决mac下总提示需要设置mldonkey url的问题。

--- trunk/chrome.extension.mldonkey/main.js
+++ trunk/chrome.extension.mldonkey/main.js
@@ -1,5 +1,9 @@
 var url=null;
 
+chrome.extension.sendRequest({getParam:"url"}, function(response){
+	url=response.url;
+});
+
 $('a[href^=ed2k]').click(function(){
 	
 	chrome.extension.sendRequest({getParam:"url"}, function(response){
@@ -7,9 +11,25 @@ $('a[href^=ed2k]').click(function(){
 	});
 	
 	if(url==null || url==""){
-		window.alert("mldonkey服务器地址为空。请设置选项中的url为mldonkey服务器地址。");
+		window.alert("must be set mldonkey server url from option page.");
 	}else{
 		window.open(url+"/submit?q="+escape(this.href),"mldonkey","width=300,height=200");
 	}
 });
+
+$('a[href$=.torrent]').click(function(){
+	
+	chrome.extension.sendRequest({getParam:"url"}, function(response){
+		url=response.url;
+	});
+	
+	if(url==null || url==""){
+		window.alert("must be set mldonkey server url from option page.");
+	}else{
+		window.open(url+"/submit?q="+escape("startbt "+this.href),"mldonkey","width=300,height=200");
+	}
+	return false;
+});
+
+
 
