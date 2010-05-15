c9377b2139d6c35d6d8ed8469c5096f7fedfa5a2
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2010-05-15 15:44:12 +0000

    实现最基本的根据isbn查出书名的功能.

--- Videos/src/main/java/com/easymorse/videos/client/event/PlayMideaEventHandler.java
+++
@@ -1,7 +0,0 @@
-package com.easymorse.videos.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface PlayMideaEventHandler extends EventHandler {
-	void onPlayMidea(PlayMideaEvent event);
-}
