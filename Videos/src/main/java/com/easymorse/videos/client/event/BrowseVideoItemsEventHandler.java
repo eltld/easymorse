a30ad6b7e72baa2145b24d6dd9380792ad9e10df
marshal.wu@gmail.com <marshal.wu@gmail.com@a7312a1b-414d-0410-90fb-b9e876aabe50>
2010-05-21 06:22:26 +0000

    基本完成了收藏夹的功能.

--- Videos/src/main/java/com/easymorse/videos/client/event/BrowseVideoItemsEventHandler.java
+++
@@ -1,7 +0,0 @@
-package com.easymorse.videos.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface BrowseVideoItemsEventHandler extends EventHandler {
-	void onBrowseVideoItem(BrowseVideoItemsEvent event);
-}
